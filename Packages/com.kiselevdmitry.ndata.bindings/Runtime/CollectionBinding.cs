484648b26413e33bb4121e1a2b4bd7a33ad9833b
Dmitry Kiselev <watermelon.developers@gmail.com>
2022-01-05 23:27:41 +0100

    Add IntPrefabBinding

--- Packages/com.kiselevdmitry.ndata.bindings/Runtime/CollectionBinding.cs
+++ Packages/com.kiselevdmitry.ndata.bindings/Runtime/CollectionBinding.cs
@@ -1,11 +1,19 @@
-﻿using KiselevDmitry.Utils;
+﻿using System.Collections.Generic;
+using KiselevDmitry.Utils;
 using NData;
 using UnityEngine;
 
 namespace KiselevDmitry.NData.Bindings {
     public class CollectionBinding : Binding {
-        public RectTransform Template;        
+        public RectTransform Template;
+        
         private Collection collection;
+        private List<RectTransform> widgets;
+
+        public override void Awake() {
+            widgets = new List<RectTransform>();
+            base.Awake();
+        }
 
         protected override void Bind() {
             var context = GetContext(Path);
@@ -16,15 +24,17 @@ namespace KiselevDmitry.NData.Bindings {
 
             collection.OnItemInsert += OnItemInsert;
             collection.OnItemsClear += OnItemsClear;
+            collection.OnItemRemove += OnItemRemove;
             for (var i = 0; i < collection.ItemsCount; ++i) {
                 OnItemInsert(i, collection.GetBaseItem(i));
             }
         }
-
+        
         protected override void Unbind() {
             if (collection == null) return;
             collection.OnItemInsert -= OnItemInsert;
             collection.OnItemsClear -= OnItemsClear;
+            collection.OnItemRemove -= OnItemRemove;
             collection = null;
             OnItemsClear();
         }
@@ -41,10 +51,18 @@ namespace KiselevDmitry.NData.Bindings {
             var itemData = widget.gameObject.AddComponent<ItemDataContext>();
             itemData.SetContext(item);
             itemData.SetIndex(position);
+            widgets.Insert(position, widget);
         }
 
+        private void OnItemRemove(int position) {
+            var widget = widgets[position];
+            widgets.RemoveAt(position);
+            GameObject.Destroy(widget.gameObject);
+        }
+        
         protected void OnItemsClear() {
             transform.DestroyChildren();
+            widgets.Clear();
         }
     }
 }
---
+++ Packages/com.kiselevdmitry.ndata.bindings/Runtime/IntPrefabBinding.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using KiselevDmitry.Utils;
+using NData;
+using UnityEngine;
+
+namespace KiselevDmitry.NData.Bindings {
+    public class IntPrefabBinding : Binding {
+        [Serializable]
+        private class IntPrefabPair {
+            public int Value;
+            public GameObject Prefab;
+        }
+
+        [SerializeField]
+        private IntPrefabPair[] pairs;
+        [SerializeField]
+        private GameObject defaultPrefab;
+        
+        private Property<int> property;
+
+        protected override void Bind() {
+            var context = GetContext(Path);
+            if (context == null) return;
+
+            property = context.FindProperty<int>(Path, this);
+            if (property != null) {
+                property.OnChange += OnChange;
+            }
+        }
+
+        protected override void Unbind() {
+            if (property != null) {
+                property.OnChange -= OnChange;
+                property = null;
+            }
+        }
+
+        protected override void OnChange() {
+            if (property == null) {
+                return;
+            }
+            
+            transform.DestroyChildren();
+            
+            var value = property.GetValue();
+            var pair = pairs.FirstOrDefault(x => x.Value == value);
+            var prefab = pair != null ? pair.Prefab : defaultPrefab;
+            
+            if (prefab != null) {
+                GameObject.Instantiate(prefab, transform);
+            }
+        }
+    }
+}
